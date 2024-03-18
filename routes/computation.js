f94490e866d22474b4689f6e874a708241f4c949
Darren Ross <78877539+djgamekid@users.noreply.github.com>
2024-03-18 00:34:28 -0500

    Updated for recalculations and multiple numbers

--- routes/computation.js
+++ routes/computation.js
@@ -1,10 +1,9 @@
 var express = require('express');
 var router = express.Router();
-var num1 = Math.floor(Math.random() * 61);
 
 /* GET computation listing. */
 router.get('/', function(req, res, next) {
-  
+  var num1 = Math.floor(Math.random() * 61);
   res.send(`Math.cos applied to ${num1} is ${Math.cos(num1)}`);
 });
 
